4e410a9d3f348959104a489f4091f5692c8368aa
Murilo <oxelot17898@gmail.com>
2026-05-10 22:19:04 -0300

    renderizando cards na div de trending

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,7 @@ const BASE_URL = 'https://api.discogs.com';
 
 let paginaAtual = 1;
 let totalPagina;
+let resultadoPagina1 = []
 
 const getAlbumPage = async (pagina = 1) =>{
     
@@ -19,21 +20,76 @@ const getAlbumPage = async (pagina = 1) =>{
 }
 
 const chamarFuncao = async (pagina) =>{ //provisório
-    
+
     const dados = await getAlbumPage(pagina)
-    renderizarAlbuns(dados.results)
+
+
+    if(pagina == 1){
+        resultadoPagina1 = dados.results
+        renderizarTrending(resultadoPagina1)
+    }
+
+    renderizarAlbums(dados.results)
     verificarPagina()
+    alterarInformacaoPaginas()
+
 }
 
-//disabilita os botões de página caso ao apertar algum deles fosse para alguma página inválida
+const renderizarTrending = (albums) =>{
+    const trendigContainer = document.getElementById('trending-list')
+    const top5 = ordenarAlbums(albums)
+
+    trendigContainer.innerHTML = top5.map(album => `
+        <div class="card">
+            <div class="img" style="
+                background-image: url('${album.cover_image}');  
+            ">
+                <div class="card-information">
+                    <div class="format"> <p>${album.format[1] ?? album.format[0]}</p> </div>
+                        <button class="wantlist">
+                            <i class="fa-regular fa-heart"></i>
+                        </button>
+                    </div>
+                    </div>
+                        <div class="album-information">
+                            <div class="name-year">
+                                <p class="nome">${album.title.split('-')[1].trim()}</p>
+                                <p class="ano">${album.year}</p>
+                            </div>
+                            <p class="artitas-nome">${album.title.split('-')[0].trim()}</p>
+                        </div>
+                        <div class="card-marketplace">
+                            <p class="classificacao">REFERENCES</p>
+                            <div class="preco">
+                                <p class="preco_card_numero">${album.style[0] ?? '—'}</p>
+                                <div class="movimentacao">
+                                    <p>${album.type}</p>
+                                </div>
+                            </div>        
+                        </div>
+                    </div>
+                </div>
+            </div>
+    `).join('')
+}
+
+const ordenarAlbums = (albums) =>{
+    return [...albums].sort((a,b) => b.community['want'] - a.community['want']).slice(0,5)
+}
+
+//desabilita modificação de páginas inválida
 function verificarPagina() {
-  document.getElementById('btn-prev').disabled = paginaAtual <= 1;
-  document.getElementById('btn-next').disabled = paginaAtual >= totalPagina;
+    document.getElementById('btn-prev').disabled = paginaAtual <= 1;
+    document.getElementById('btn-next').disabled = paginaAtual >= totalPagina;
+}
+
+const alterarInformacaoPaginas = () =>{
+    document.getElementById('info').textContent = `Página ${paginaAtual} de ${totalPagina}`;
 }
 
 
 //cria os cards com innerHTML
-const renderizarAlbuns = (albums) =>{
+const renderizarAlbums = (albums) =>{
     const container = document.getElementById('explorer-list')
 
     container.innerHTML = albums.map(album => `
